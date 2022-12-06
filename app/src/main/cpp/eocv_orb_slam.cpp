381e2914abdd2a21b60072fd1f4b5900a4383b26
Yey007 <yey007tech@gmail.com>
2022-12-06 11:15:56 -0500

    Initial commit

---
+++ app/src/main/cpp/eocv_orb_slam.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("eocv_orb_slam");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("eocv_orb_slam")
+//      }
+//    }
