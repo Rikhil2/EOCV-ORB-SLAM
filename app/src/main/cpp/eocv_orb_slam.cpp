8b0b328d0eb509a1a915de5b40ee8098031e25da
Yey007 <yey007tech@gmail.com>
2022-12-08 11:57:10 -0500

    Builds... but there's probably a better way

--- app/src/main/cpp/eocv_orb_slam.cpp
+++ app/src/main/cpp/eocv_orb_slam.cpp
@@ -1,3 +1,5 @@
+#include "orb-slam3/include/System.h"
+
 // Write C++ code here.
 //
 // Do not forget to dynamically load the C++ library into your application.
@@ -15,3 +17,7 @@
 //         System.loadLibrary("eocv_orb_slam")
 //      }
 //    }
+
+void hello_world() {
+    ORB_SLAM3::System s("", "", ORB_SLAM3::System::IMU_MONOCULAR, false);
+}
