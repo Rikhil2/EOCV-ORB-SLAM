0b8e12f3143d269bdfc56f154ba3739733be5444
Yey007 <yey007tech@gmail.com>
2022-12-08 20:58:29 -0500

    Start examples project

---
+++ example/src/main/java/org/firstinspires/ftc/teamcode/OrbSlamPipeline.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class OrbSlamPipeline extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
